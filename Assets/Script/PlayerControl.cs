9549450d13073a8d039c91d0e03f553662d9dd47
Ekiz24 <tkboxnn@gmail.com>
2024-02-28 20:05:41 +0200

    Initial commit

---
+++ Assets/Script/PlayerControl.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    private Animator animator;
+    private Rigidbody2D rigidbody2d;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        rigidbody2d = GetComponent < Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        //When player press left or right
+        if (horizontal != 0)
+        {
+            animator.SetFloat("Horizontal", horizontal);
+            animator.SetFloat("Vertical", 0);
+        }
+        //When player press up or down
+        if (vertical != 0)
+        {
+            animator.SetFloat("Vertical", vertical);
+            animator.SetFloat("Horizontal", 0);
+        }
+        //if players change stand to move
+        Vector2 direction = new Vector2(horizontal, vertical);
+        animator.SetFloat("Speed", direction.magnitude);
+
+        //move character to the direction
+        rigidbody2d.velocity = direction * 0.6f;
+
+
+    }
+}
