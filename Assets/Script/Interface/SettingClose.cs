1e47e677c300b09edaced8cfc5063a09d9b644c6
Ekiz24 <tkboxnn@gmail.com>
2024-03-20 21:58:11 +0200

    Removed extraneous files. Fixed problems on the start screen.

---
+++ Assets/Script/Interface/SettingClose.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingClose : MonoBehaviour
+{
+    public GameObject Close;
+
+
+    public void CloseSetting()
+    {
+        Close.SetActive(false);
+    }
+
+}
